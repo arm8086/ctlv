c75e78b464b586d8d24322f413cb23ac8c3c2e37
x32 <72319973+problematic-scripter@users.noreply.github.com>
2023-01-07 13:19:36 +0000

    Create git.lua

---
+++ git.lua
@@ -0,0 +1,20 @@
+local settings_ = {...}
+local username = settings_[1]
+local repo = settings_[2]
+local token = settings_[3]
+local http = game:GetService("HttpService")
+function GetFile(FilePath)
+	local url = string.format('https://api.github.com/repos/%s/%s/contents/%s', username, repo, FilePath)
+	local response = http:RequestAsync(
+		{
+			Url = url,
+			Method = "GET",
+			Headers = {
+				["Content-Type"] = "application/json",
+				Accept = 'application/vnd.github.v3',
+				Authorization = string.format('token %s', token)
+			},
+		}
+	)
+	return(response.Body)
+end
