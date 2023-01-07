d20037bb01a8c728710b76169580dae472d0350f
x32 <72319973+problematic-scripter@users.noreply.github.com>
2023-01-07 13:30:36 +0000

    Update git.lua

--- git.lua
+++ git.lua
@@ -19,4 +19,19 @@ function git.GetFile(FilePath)
 	)
 	return http:GetAsync(http:JSONDecode(response.Body).download_url)
 end
+function git.PutFile(FilePath, Contents)
+	local url = string.format('https://api.github.com/repos/%s/%s/contents/%s', username, repo, FilePath)
+	local response = http:RequestAsync(
+		{
+			Url = url,
+			Method = "PUT",
+			Headers = {
+				["Content-Type"] = "application/json",
+				Authorization = string.format('Bearer %s', token)
+			},
+			Body = http:JSONEncode({message = "Upload from console", content = Contents})
+		}
+	)
+	return http:JSONDecode(response.Body)
+end
 return git
